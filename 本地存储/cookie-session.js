088e74431885db95a14c155d10261607327879df
474670539 <18231319486@163.com>
2018-11-06 16:22:38 +0800

    本地存储

---
+++ 本地存储/cookie-session.js
@@ -0,0 +1,8 @@
+// cookie
+document.cookie;
+// 添加cookie
+document.cookie = "name=wangxiumin"
+// 修改cookie
+document.cookie = "name=wangxiumin1"
+
+// session
---
+++ 本地存储/local.js
@@ -0,0 +1,8 @@
+// 添加localstorage
+window.localStorage.setItem("name","wangxiumin");  //添加
+window.localStorage.getItem("name");    //获取
+window.localStorage.removeItem("name");  //删除
+// sessionStorage
+window.sessionStorage.setItem("name","wangxiumin");  //添加
+window.sessionStorage.getItem("name");    //获取
+window.sessionStorage.removeItem("name");  //删除
